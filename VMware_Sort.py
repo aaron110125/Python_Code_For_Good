c5d0f2ab04f6f814b946670423d3ffb93cae0798
Aaron D'Costa <dcostaaaron@yahoo.in>
2022-07-07 15:08:31 -0700

    Create VMware_Sort.py

---
+++ VMware_Sort.py
@@ -0,0 +1,20 @@
+vmware_string = 'Vm1w3a5re'
+
+def string_sort(vmware_string):
+    final_char = []
+    num = []
+    char = []
+    google_string = list("".join(vmware_string))
+
+    for i in vmware_string:
+
+        if i.isalpha():
+            char.append(i)
+        else:
+            num.append(i)
+
+    final_char = "".join(char + num)
+    print(final_char)
+
+string_sort(vmware_string)
+
