7a54398a785b0aa80ddffb90edfef7f299abc285
Aaron D'Costa <dcostaaaron@yahoo.in>
2022-11-30 21:45:21 -0800

    Update inherit_juni.py

--- inherit_juni.py
+++ inherit_juni.py
@@ -1,23 +1,52 @@
 #testing
 import logging
 
-class DeviceRouter(object):
+class DeviceSwitch:
+    def __init__(self,device_model,device_id):
+        self.device_model  = device_model
+        self.device_id = device_id
 
-    def __init__(self,router_name,router_id):
-        self.router_name = router_name
-        self.router_id = router_id
+    def device_info(self):
+        print(f"Name of router model is {self.device_model} and device id is {self.device_id}")
 
-    def display(self):
-        logging.info(f"Name of router is {self.router_name} and router id of the router is {self.router_id}")
-        print(self.router_name, self.router_id)
+    def device_display(self):
+        print("These are the device models")
 
-class DeviceCore(DeviceRouter):
-    def Print(self):
-        logging.info("Printing device core functionality")
+class Vxlan(object):
 
-Switch = DeviceCore("Rubrix",48328)
-Switch.display()
-Switch.Print()
-# Driver code
-Cisoc = DeviceRouter("Junos234", 1342) # An Object of Router
-Cisoc.display()
+    def __init__(self,vxlan_name):
+        self.vxlan_name = vxlan_name
+
+    def get_vxlan_name(self):
+        return self.vxlan_name
+
+    def is_same_network(self):
+        return False
+
+class EVxlan(Vxlan):
+    def is_same_network(self):
+        return True
+
+class Pointer_Image(object):
+    def __init__(self,x = 0, y = 0):
+        self.x = x
+        self.y = y
+
+    def image_distance(self):
+        """ Find distance from the origin """
+        return (self.x**2 + self.y**2) ** 0.5
+
+#Driver code
+vxlan1 = Vxlan("1004")
+print(vxlan1.get_vxlan_name(),vxlan1.is_same_network())
+vxlan2 = Vxlan("1005")
+print(vxlan2.get_vxlan_name(),vxlan2.is_same_network())
+
+p1 = Pointer_Image(5,12)
+print("Image distance from the origin is {} m".format(p1.image_distance()))
+
+device1 = DeviceSwitch("Nokia","ID-3XS")
+device2 = DeviceSwitch("Juniper","Junos3553")
+print(device1.device_info())
+print(device2.device_display())
+#logging.info("Printing device core functionality")
